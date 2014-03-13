83ca9dd070043aaf0569e852512a1d9d36a35edf
U-NA\john.dodson <john.dodson@NYJDODSON2.NA.weightwatchers.net>
2014-03-13 16:21:57 -0400

    Initial commit of skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LittleRedBean::Application.config.secret_key_base = 'cc3ecd5b454cb8a30697af2b0e20bd414d504220244d995f07368e8e4dfa41c82e3a1da60aecef46350fddbea47803efb1aa86cd662f45fccb88cfeb0cba2415'
